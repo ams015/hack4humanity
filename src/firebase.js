8d2ba892de33c86c8e618a27cf133cb7d423ce2e
“shubham.vyas” <vyasshubham.41@gmail.com>
2023-02-18 13:37:26 -0800

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC5aLFgt95ND-kLvwuG_pRv3uf3Py1etwo",
+    authDomain: "hack4humanity-f40d2.firebaseapp.com",
+    projectId: "hack4humanity-f40d2",
+    storageBucket: "hack4humanity-f40d2.appspot.com",
+    messagingSenderId: "386131252398",
+    appId: "1:386131252398:web:57dfbb22d80d19553ec93f",
+    measurementId: "G-5FKNF5EGNH"
+  };
