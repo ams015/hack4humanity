65b0b1fd5f992fba3cc35d9d723e381f245ca9b0
smitpatel24 <smit@gopikunj.com>
2023-02-18 14:02:38 -0800

    Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,72 @@
 import React from "react";
+import { useState } from "react";
+import { Box, TextField, Button } from "@mui/material";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`);
+  }
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        bgcolor: "#f5f5f5",
+      }}
+    >
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          maxWidth: "400px",
+          bgcolor: "white",
+          padding: "20px",
+          borderRadius: "5px",
+          boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <TextField
+          label="Username"
+          variant="outlined"
+          value={username}
+          onChange={handleUsernameChange}
+          margin="normal"
+          sx={{ mb: "20px" }}
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          margin="normal"
+          sx={{ mb: "20px" }}
+        />
+        <Button type="submit" variant="contained">
+          Login
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default Login;
