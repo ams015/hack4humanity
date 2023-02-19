850aa6d7e4feffc0d2ff0d842d967ee03dd81428
smitpatel24 <smit@gopikunj.com>
2023-02-18 18:46:55 -0800

    location enabled

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Map = () => {
+  const [location, setLocation] = useState({ latitude: null, longitude: null });
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      setLocation({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+      console.log(position.coords.latitude, position.coords.longitude);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
   return (
     // <div className="map-container">
     //   <iframe
