a5464e6794c6329a1e37e88f67ec0dcafe61b22a
smitpatel24 <smit@gopikunj.com>
2023-02-18 18:30:43 -0800

    Add map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Router, Route, Link, useNavigate, Routes } from "react-router-dom";
 import Login from "./Login";
+import Map from "./Map";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />}></Route>
+      <Route path="/map" element={<Map />}></Route>
     </Routes>
   );
 }
---
+++ src/Map.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Map = () => {
+  return (
+    // <div className="map-container">
+    //   <iframe
+    //     title="Google Map"
+    //     width="600"
+    //     height="450"
+    //     frameBorder="0"
+    //     style={{ border: 0 }}
+    //     src={`https://www.google.com/maps/embed/v1/view?key=YOUR_API_KEY&center=${latitude},${longitude}&zoom=15`}
+    //     allowFullScreen
+    //   />
+    // </div>
+    <div>map test</div>
+  );
+};
+
+export default Map;
